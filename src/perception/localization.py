46a13b347734043cc6b38bf874f3ae835df05a81
Minh Quan Vu <59466039+minhquanvu0604@users.noreply.github.com>
2024-04-18 16:41:06 +1000

    Delete src/perception/localization.py

--- src/perception/localization.py
+++
@@ -1,2 +0,0 @@
-# @TODO: Transiting the realsense handling module to this scripts for oop handle
-# @TODO: Setup tf_broadcaster here to broadcast the tf from bottle dectected related to camera frame
