97d5257b90d050896e4cbdce3fbb9ff5e56d3c47
anhtungnguyen <anhtungnguyen1809@gmail.com>
2024-04-27 20:56:15 +1000

    add get yaw

--- src/perception/real_time_detect_using_model.py
+++ src/perception/real_time_detect_using_model.py
@@ -9,6 +9,9 @@ import pyrealsense2 as rs
 class RealSense:
 
     def __init__(self):
+        self.get_crate = False
+        self.get_bottle = False
+
         self.bridge = CvBridge()
         self.imageSub = rospy.Subscriber("/camera/color/image_raw", Image, self.rgb_callback)
         self.depthSub = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.depth_callback)
@@ -37,17 +40,19 @@ class RealSense:
         self.depth_intrinsics.model = rs.distortion.none  # Assuming no lens distortion
         self.depth_intrinsics.coeffs = [0, 0, 0, 0, 0]  # Assuming no distortion coefficients
 
-        self.get_crate = False
-        self.get_bottle = False
-
         self.crate_pos = None
         self.bottle_pos = None
+
+        self.yaw = None
 
     def get_crate_pos(self):
         return self.crate_pos
     
     def get_bottle_pos(self):
         return self.bottle_pos
+    
+    def get_yaw(self):
+        return self.yaw
 
     # Projecting a 2D point to a 3D image plane:
     def project_2D_to_3D(self, u, v, depth):
@@ -58,7 +63,7 @@ class RealSense:
 
         # translation of camera 
         tx = -0.0329  # unit: meters
-        ty = 0.0555
+        ty = 0.0755
         tz = -0.0632
             
         # Calculate the 3D point (with end of effector frame)
@@ -66,11 +71,11 @@ class RealSense:
         y = (v - cy) * depth / fy
         
         #for UR3
-        # x = x + tx
-        # y = y + ty
-        # depth = depth - tz
+        x = (x + tx)*-1
+        y = (y + ty)*-1
+        depth = depth - tz
 
-        P_camera = np.array([x, y, depth, 1])  # 3D point in the camera frame
+        P_camera = np.array([x, y, depth, self.yaw])  # 3D point in the camera frame
 
         
         return P_camera
@@ -79,6 +84,8 @@ class RealSense:
         # Convert the ROS Image message to a cv2 image
         self.rgb_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         
+        tung_x = None
+        tung_y = None
 
         if (self.get_crate == True and self.get_bottle == False):
             self.img_processing()
@@ -99,6 +106,8 @@ class RealSense:
                     # Calculate the center of the bounding box
                     center_x_bottle = int((x1 + x2) / 2)
                     center_y_bottle = int((y1 + y2) / 2)
+                    tung_x = center_x_bottle
+                    tung_y = center_y_bottle
                     cv2.circle(self.rgb_image, (center_x_bottle, center_y_bottle), 5, (0, 255, 0), -1)  # Change the color and size as needed
                     # Make sure the depth image is already available and synced with the current RGB frame
                     if self.depth_image is not None:
@@ -120,7 +129,12 @@ class RealSense:
         
         else:
             pass
-
+        
+        # cv2.circle(self.rgb_image, (tung_x, tung_y), 5, (0, 255, 0), -1)
+        
+        width = int(self.rgb_image.shape[1] * 1.5)
+        height = int(self.rgb_image.shape[0] * 1.5)
+        self.rgb_image = cv2.resize(self.rgb_image, (width, height))
         # Display the image
         cv2.imshow('YOLO Detection', self.rgb_image)
         # cv2.imshow('Depth Detection', self.depth_image)
@@ -182,7 +196,7 @@ class RealSense:
         if contours is not None:
             # print("contour")
             # Draw the contour itself (just for visualization)
-            cv2.drawContours(img, [crate_contour], -1, (0, 255, 0), 3)
+            # cv2.drawContours(img, [crate_contour], -1, (0, 255, 0), 3)
 
             # Calculate the bounding rectangle, which gives us the four points
             rect = cv2.minAreaRect(crate_contour)
@@ -217,7 +231,8 @@ class RealSense:
             if angle > 45:
                 angle -= 90
 
-            # print(angle)
+            print("angle:", angle)
+            self.yaw = angle
 
             # The order of box points: bottom-left, top-left, top-right, bottom-right
             bottom_left, top_left, top_right, bottom_right = box
@@ -236,6 +251,7 @@ if __name__ == '__main__':
     # Create the RealSense object
     rs = RealSense()
 
+    # rs.get_bottle = True
     rs.get_bottle = True
 
     # Spin to keep the script for exiting
